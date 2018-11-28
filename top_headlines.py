a0ec8172c22388a1a679f25c7183b2e917e2755c
ebwydra <ebwydra@gmail.com>
2018-11-28 11:05:04 -0500

    extra credit pt 1

--- top_headlines.py
+++ top_headlines.py
@@ -3,6 +3,14 @@ import requests
 from secrets import api_key
 import json
 
+def get_section_headlines(section):
+    params = {}
+    base_url = "https://api.nytimes.com/svc/topstories/v2/{}.json".format(section)
+    params["api-key"] = api_key
+    resp = requests.get(base_url, params = params)
+    resp_dict = json.loads(resp.text)
+    return resp_dict
+
 def get_tech_headlines():
     params = {}
     base_url = "https://api.nytimes.com/svc/topstories/v2/technology.json"
@@ -10,16 +18,6 @@ def get_tech_headlines():
     resp = requests.get(base_url, params = params)
     resp_dict = json.loads(resp.text)
     return resp_dict
-
-nyt_data = get_tech_headlines()
-
-list_of_articles = []
-
-for result in nyt_data['results'][0:5]:
-    title = result['title']
-    url = result['url']
-    article = title + ' ({})'.format(url)
-    list_of_articles.append(article)
 
 app = Flask(__name__)
 
@@ -32,7 +30,33 @@ def hello_world():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name = name, my_list = list_of_articles)
+
+    section = "technology"
+    nyt_data = get_section_headlines(section)
+
+    list_of_articles = []
+
+    for result in nyt_data['results'][0:5]:
+        title = result['title']
+        url = result['url']
+        article = title + ' ({})'.format(url)
+        list_of_articles.append(article)
+
+    section_list = ["home", "opinion", "world", "national", "politics", "upshot", "nyregion", "business", "technology", "science", "health", "sports", "arts", "books", "movies", "theater", "sundayreview", "fashion", "tmagazine", "food", "travel", "magazine", "realestate", "automobiles", "obituaries", "insider"]
+
+    return render_template('user.html', name = name, section = section, my_list = list_of_articles, list_of_sections = section_list)
+
+@app.route('/user/<name>/<section>')
+def section(name, section):
+    list_of_articles2 = []
+    section_data = get_section_headlines(section)
+    for result in section_data['results'][0:5]:
+        title = result['title']
+        url = result['url']
+        article = title + ' ({})'.format(url)
+        list_of_articles2.append(article)
+    section_list = ["home", "opinion", "world", "national", "politics", "upshot", "nyregion", "business", "technology", "science", "health", "sports", "arts", "books", "movies", "theater", "sundayreview", "fashion", "tmagazine", "food", "travel", "magazine", "realestate", "automobiles", "obituaries", "insider"]
+    return render_template('user.html', name = name, section = section, my_list = list_of_articles2, list_of_sections = section_list)
 
 if __name__ == '__main__':
     print('starting Flask app', app.name)
